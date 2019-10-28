c778e6a907c8b14020c834e02d6ae61e3abcfa64
ttt-tyme <57058935+ttt-tyme@users.noreply.github.com>
2019-10-28 20:56:04 +0100

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,3 +1,8 @@
 function swap(sheet) {
 	document.getElementById("style").setAttribute("href", sheet);
+	for (var i = 0; i < titles; i++) {
+ divHeights("t"+[i]);
+  }
+  for (var i = 0; i < paragraphs; i++) {
+ divHeights("p"+[i]);
 }
