076a0c01b85c5f9ae62a37e8eef24f96550ae233
ttt-tyme <57058935+ttt-tyme@users.noreply.github.com>
2019-10-28 22:00:22 +0100

    Update themes.js

--- themes.js
+++ themes.js
@@ -5,10 +5,15 @@ function swap(sheet) {
 const titles = 5;
 const paragraphs = 466;
 
-for (var i = 0; i < titles; i++) {
+
+setTimeout(function(){
+  for (var i = 0; i < titles; i++) {
+    hoverElement("t"+[i]);
  divHeights("t"+[i]);
   }
   for (var i = 0; i < paragraphs; i++) {
+    hoverElement("p"+[i]);
  divHeights("p"+[i]);
-}
+  }
+},300); 
 }
